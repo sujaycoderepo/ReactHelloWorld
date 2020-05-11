05f75febfa37b79dcfe86225aa2b08b3dcb2ba00
SujayM <SujayMane@Hotmail.com>
2020-05-11 12:34:57 +0530

    Check with css from site

--- src/index.js
+++ src/index.js
@@ -3,19 +3,39 @@ import ReactDOM from 'react-dom'
 
 const App = () =>{
     return(
-        <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar"></img>
+        // <div className="ui comments">
+        //     <div className="comment">
+        //         <a href="/" className="avatar">
+        //             <img alt="avatar"></img>
+        //         </a>
+        //         <div className="content">
+        //             <a  href="/" className="author">
+        //                 Test
+        //             </a>
+        //             <div className="metadata">
+        //                 <span className="date">Today 6PM</span>
+        //             </div>
+        //             <div className="text">Nice one</div>
+        //         </div>
+        //     </div>
+        // </div>
+        <div class="ui comments">
+            <h3 class="ui dividing header">Comments</h3>
+            <div class="comment">
+                <a href="/" class="avatar">
+                    <img alt="avatar" src="/images/avatar/small/matt.jpg"></img>
                 </a>
-                <div className="content">
-                    <a  href="/" className="author">
-                        Test
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today 6PM</span>
+                <div class="content">
+                    <a href="/" class="author">Matt</a>
+                    <div class="metadata">
+                        <span class="date">Today at 5:42PM</span>
                     </div>
-                    <div className="text">Nice one</div>
+                    <div class="text">
+                        How artistic!
+                    </div>
+                    <div class="actions">
+                        <a href="/" class="reply">Reply</a>
+                    </div>
                 </div>
             </div>
         </div>
