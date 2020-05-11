a6ac942bd719b6f5564df52f8819ff3028b360b1
SujayM <SujayMane@Hotmail.com>
2020-05-11 10:58:15 +0530

    AddedDate

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+function getTime(){
+    return (new Date()).toLocaleDateString();
+}
+
 // Create react component 
 const App = () =>{
     const bottonText = "Click Me";
+    let currentTime = getTime(); 
     return(
         <div>
+            {currentTime}
             <label className="lable" htmlFor="name">Enter Name:</label>
             <input id="name" type="text"/>
             <button style = {{backgroundColor:'blue', color:'white'}}> {bottonText}</button>
