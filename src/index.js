8799dbe6808301854f46e2b74642bdf570ca86d8
SujayM <SujayMane@Hotmail.com>
2020-05-14 13:56:05 +0530

    First_Class_Based_Component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-const App = () =>{
-    window.navigator.geolocation.getCurrentPosition(
-        position => console.log(position),
-        err => console.log(err)
-    );
-    
-    return(
-     <div>
-            
-     </div>
- );
+class App extends React.Component{
+    render(){
+        window.navigator.geolocation.getCurrentPosition(
+            position => console.log(position),
+            err => console.log(err)
+        );
 
+        return (<div></div>);
+    }
 }
 
 ReactDom.render(<App/>, document.querySelector("#root"));
