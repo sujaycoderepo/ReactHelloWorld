5a086e075e8d1664717cbe1208b3b70e765a36d6
SujayM <SujayMane@Hotmail.com>
2020-05-26 23:07:11 +0530

    Code_Refactoring_JsObject

--- src/seasonDisply.js
+++ src/seasonDisply.js
@@ -10,23 +10,42 @@ const getSeason = (lat, month) => {
     }
 }
 
-const getMessage = (season) => {
-    return season === 'winter' ? 'Its Cold !!!!' : 'Its too Hot !!!! ';
+//// CODE REFACTORING by using JS Object 
+// const getMessage = (season) => {
+//     return season === 'winter' ? 'Its Cold !!!!' : 'Its too Hot !!!! ';
+// }
+
+// const getIcon = (season) =>{
+//     return season === 'winter' ? 'snowflake' : 'sun';
+// }
+
+//// JS Object 
+const seasonConfig = {
+    summar : {
+        message : 'Its Too Hot !!!!',
+        iconName : 'sun'
+    },
+    winter : {
+        message : 'Its Cold !!!!',
+        iconName: 'snowflake'
+    }
 }
 
-const getIcon = (season) =>{
-    return season === 'winter' ? 'snowflake' : 'sun';
-}
 
 const seasonDisplay = (props) =>{
 
     const season = getSeason(props.lat, new Date().getMonth());
-
+    const {message, iconName}  = seasonConfig[season]
     return(
         <div>
-            <i className = {`${getIcon(season)} icon`}/>
+            {/* <i className = {`${getIcon(season)} icon`}/>
             <h1>{getMessage(season)}</h1>
-            <i className = {`${getIcon(season)} icon`}/>
+            <i className = {`${getIcon(season)} icon`}/> */}
+
+            <i className = {`${iconName} icon`}/>
+            <h1>{message}</h1>
+            <i className = {`${iconName} icon`}/>
+
         </div>
     );
 }
