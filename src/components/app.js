f6fafe32565832ab8c5dcc101250bdff8bcd4e70
SujayM <SujayMane@Hotmail.com>
2020-05-27 21:21:30 +0530

    Project_init

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchBar from './searchBar/searchBar'
+
+const App = () =>{
+    return(
+        <div>
+            <SearchBar/>    
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/searchBar/searchBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class searchBar extends React.Component{
+
+    render(){
+        return(
+            <div>
+                 Search Bar   
+            </div>
+        )
+    }
+}
+
+export default searchBar;
