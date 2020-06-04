5fa292452c2a22358dd674741e04f9e60038c2c4
SujayM <SujayMane@Hotmail.com>
2020-06-04 20:31:06 +0530

    onSubmit_On_EnterKey_Works for Single_Text_issue

--- src/components/searchBar/searchBar.js
+++ src/components/searchBar/searchBar.js
@@ -26,10 +26,13 @@ class searchBar extends React.Component{
                         value={this.state.term}
                         onChange={(event)=> {this.setState({term : event.target.value})}}></input>
                     </div>
-                    {/* <div className="field">
+                    {/* 
+                    ////https://github.com/Shopify/polaris-react/issues/168
+                    <div className="field">
                         <label>Image URL</label>
                         <input type="text" onChange = {(event) => console.log(event.target.value)}></input>
                     </div> */}
+
                  </form>
             </div>
         )
